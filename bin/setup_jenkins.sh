55719ecb66700c471641bf740556f9606e763e6f
kavinwindows <56716904+kavinwindows@users.noreply.github.com>
2019-10-18 17:18:04 +0530

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,18 +13,64 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-# TBD
+oc new-app jenkins-persistent \
+	--param ENABLE_OAUTH=true \
+	--param MEMORY_LIMIT=4Gi \
+	--param VOLUME_CAPACITY=4Gi \
+	--param DISABLE_ADMINISTRATIVE_MONITORS=true \
+	-n ${GUID}-jenkins
+
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1 -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
-# TBD
+oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+	USER root\n\
+	RUN yum -y install skopeo \
+	&& yum clean all\n
+	USER 1001' \
+	--name=jenkins-agent-appdev \
+	-n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-# TBD
+#oc new-build ${REPO} \
+#	--name=tasks-pipeline \
+#	--context-dir=openshift-tasks \
+#	--strategy=pipeline \
+#	--env GUID=${GUID} --env REPO=${REPO} --env CLUSTER=${CLUSTER} \
+#	-n ${GUID}-jenkins
+
+echo "apiVersion: v1
+items:
+- kind: "BuildConfig"
+  apiVersion: "v1"
+  metadata:
+    name: "tasks-pipeline"
+  spec:
+    source:
+      type: "Git"
+      git:
+        uri: ${REPO}
+      contextDir: "openshift-tasks"
+    strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+        jenkinsfilePath: Jenkinsfile
+        env:
+        - name: "GUID"
+          value: ${GUID}
+        - name: "REPO"
+          value: ${REPO}
+        - name: "CLUSTER"
+          value: ${CLUSTER}
+    triggers: []
+kind: List
+metadata: []" | oc create -f - -n ${GUID}-jenkins
+
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
   echo "Checking if Jenkins is Ready..."
-  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')
+  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.readyReplicas}')
   if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
     echo "...Yes. Jenkins is ready."
     break
